576ed463b7d954d7f4b3a666f9af92fe037a7377
Emanuele  Campos <88864112+ecampos14@users.noreply.github.com>
2023-02-28 19:14:43 -0300

    Add files via upload

---
+++ spring-labiv20231-master/src/main/java/br/gov/sp/fatec/springlabiv20231/SpringLabiv20231Application.java
@@ -0,0 +1,13 @@
+package br.gov.sp.fatec.springlabiv20231;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringLabiv20231Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringLabiv20231Application.class, args);
+	}
+
+}
---
+++ spring-labiv20231-master/src/test/java/br/gov/sp/fatec/springlabiv20231/SpringLabiv20231ApplicationTests.java
@@ -0,0 +1,13 @@
+package br.gov.sp.fatec.springlabiv20231;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringLabiv20231ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
