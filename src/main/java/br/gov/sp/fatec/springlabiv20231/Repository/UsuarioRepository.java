8486e819bdfcb18b6eda398f483c81a57a8ded4f
Emanuele  Campos <emanuelecampos14@gmail.com>
2023-02-28 23:56:57 +0000

    modal usuario

---
+++ src/main/java/br/gov/sp/fatec/springlabiv20231/Repository/UsuarioRepository.java
@@ -0,0 +1,7 @@
+package br.gov.sp.fatec.springlabiv20231.Repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import br.gov.sp.fatec.springlabiv20231.entity.Usuario;
+public interface UsuarioRepository extends JpaRepository <Usuario, Long> {
+    
+}
